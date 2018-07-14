7e66b7349f7c18dd1bff6c4defa4120390780fd4
Sophie <soph.bohr22@gmail.com>
2018-07-14 13:55:05 -0500

    JS can tell when the user chooses a correct response, bug w/ radio btn

---
+++ BroadwayKnowledgeTest/Controllers/QuizTopicsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BroadwayKnowledgeTest.Controllers
+{
+    public class QuizTopicsController : Controller
+    {
+        public IActionResult Actors()
+        {
+            return View();
+        }
+
+        public IActionResult Playbills()
+        {
+            return View();
+        }
+
+        public IActionResult Songs()
+        {
+            return View();
+        }
+
+        public IActionResult Theatres()
+        {
+            return View();
+        }
+
+        public IActionResult Tonys()
+        {
+            return View();
+        }
+
+    }
+}
